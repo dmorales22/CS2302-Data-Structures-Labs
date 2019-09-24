8bae3e966d1f2f93c936da1104dabcefb996ccdd
dmorales22 <50034808+dmorales22@users.noreply.github.com>
2019-09-24 16:52:20 -0600

    Add files via upload

--- LAB2B/Node.py
+++ LAB2B/Node.py
@@ -3,7 +3,7 @@
 #Instructor: Diego Aguirre 
 #T.A: Gerardo Barraza
 #Assignment: Lab 2 - Option B 
-#Last Modification: 09/19/2019 (Part 1 only finished) 
+#Last Modification: 09/23/2019
 #Purpose: Finds most common passwords from list. 
 
 
@@ -17,7 +17,7 @@ class Node(object): #Basic constructor of the Node class provided to us.
 		self.count = count
 		self.next = next
 		
-#This is part 1 solution (A). It is implemented and done. 
+#This is part 1. It uses solution A. 
 class LList(object): #The linked list node constructor that comes with Node Head and Int size variables. 
 	head = Node("", -1, None) 
 	size = 0 
@@ -25,16 +25,27 @@ class LList(object): #The linked list node constructor that comes with Node Head
 	def __init__(self): 
 		self.head = Node("", -1, None) 
 		self.size = 0 
-		
-	def append_passLList(self, str): #Adds nodes to link list, but also checks if the password(s) appears more than one. 
-		node = Node(str, 1, None)
-		iter = self.head  
-		
-		while iter.next != None:
-			iter = iter.next 
-			if (iter.password == str): #This checks if the password from the node is the same with the string. Adds to count. 
-				iter.count += 1 
-		if (iter.count < 2): #Only adds node to link list if the count is less than two. Updates size. 
-			iter.next = node 
-			self.size += 1
+	
+	def append_passLList(self, str): #Adds nodes to link list (at the end), but also checks if the password(s) appears more than one.
+		if (self.head.count == -1): #Assigns first node to head. 
+			node = Node(str, 1, None) 
+			self.head = node 
+			self.size += 1 
+			
+		else: 
+			node = Node(str, 1, None)
+			iter = self.head  
+			
+			while (iter.next != None):
+				if (iter.password == str): #This checks if the password from the node is the same with the string. Adds to count. 
+					iter.count += 1 
+					break
+				iter = iter.next
+				if (iter.password == str): #Another if statement to check the next node. 
+					iter.count += 1 
+					break
+					
+			if (iter.count < 2): #Only adds nodes to link list if the count is less than two to avoid duplicates. Updates size. 
+				iter.next = node  
+				self.size += 1
 			
--- LAB2B/pass.py
+++ LAB2B/pass.py
@@ -3,32 +3,59 @@
 #Instructor: Diego Aguirre 
 #T.A: Gerardo Barraza
 #Assignment: Lab 2 - Option B 
-#Last Modification: 09/19/2019 (Part 1 only finished) 
+#Last Modification: 09/23/2019  
 #Purpose: Finds most common passwords from list. 
 
 import Node
-def common_pass(passLList): #This is part 2 solution B. It is still WIP 
-	#TODO 1: add sorting method 
-	sorted_list = Node.LList() 
+def common_pass(passLList): #This is part 2 solution A. It is uses bubble sort, and uses a loop to print the top 20 nodes.  
+	temp = Node.Node("", -1, None) 
 	iter = passLList.head 
 	
-	#while (iter.next != None): 
+	for i in range(passLList.size): #Bubble sort algorithm. 
+		iter = passLList.head
+		while (iter.next != None): 
+			nextNode = iter.next 
+			if (iter.count < nextNode.count):
+				temp.password = iter.password #Switches password and count values. 
+				temp.count = iter.count 
+				
+				iter.password = nextNode.password 
+				iter.count = nextNode.count 
+				
+				nextNode.password = temp.password
+				nextNode.count = temp.count 
+			iter = iter.next 
+		
+	iter = passLList.head 
+	counter = 1
+	counter_str = ""
+	
+	print("1. ", iter.password, "Count:", iter.count) #Prints head. 
+	while (iter.next != None): #Prints elements of the nodes in a nice format until counter goes above 19. 
+		if (counter > 19): 
+			break 
+		iter = iter.next
+		counter += 1
+		counter_str = str(counter) + ". " 
+		print(counter_str, iter.password, "Count:", iter.count)  
 
-def parse_list(): #This is part 1 solution (A). It is implemented and done. 
+def parse_list(): #This is part 1 solution (A). It is implemented and done. Parsing textfile and uses append method from Node.py. 
 	print("Input textfile name: ") 
 	filename = input() 
 	passLList = Node.LList() 
 	
 	with open(filename, encoding='windows-1252') as textFile: 
+		print("Printing the top 20. This may take a while... \n") 
 		for line in textFile:
-			str = line.split() #Reads the textfile line by line so it won't kill the computer's memory. 
-			if (len(str) != 1): #If statement used if password is blank to prevent out of bound errors. 
-				passLList.append_passLList(str[1])  #Uses Append method from Node.py to add the strings to a link list. 
-				print(str[1])
+			str = line.split() #Reads the textfile line by line so it won't kill the computer's memory.
+			if (len(str) == 1): #For single column password lists. 
+				passLList.append_passLList(str[0])
+			if (len(str) != 1): #If statement used if password is blank to prevent out of bound errors.
+				passLList.append_passLList(str[1])  #Uses append_passLList method from Node.py to add the strings to a link list. 
 	
 	return passLList
 	
-def main(): 
+def main():
 	passLList = parse_list() 
 	common_pass (passLList) 
 	
